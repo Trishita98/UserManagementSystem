a09c2b1e5d99e5e3dd9d1e0a3d354757f6212057
Trishita.Das <Trishita.Das@GEMGN-220444>
2022-12-19 16:44:06 +0530

    Add project files.

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Data.SqlClient;
+
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con= new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void ConnectionString()
+        {
+            con.ConnectionString = "data source=10.50.18.16; database=training_db; integrated security=SSPI;";
+        }
+
+        public ActionResult Verify(Account acc)
+        {
+            ConnectionString();
+            con.Open();
+            
+            com.Connection = con;
+            com.CommandText = "select * from";
+            dr = com.ExecuteReader();
+            if(dr.Read())
+            {
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+            con.Close();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/TsignupController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TsignupController : Controller
+    {
+        // GET: Tsignup
+        public ActionResult Index()
+        {
+            using (training_dbEntities dbmodel = new training_dbEntities())
+            {
+                return View(dbmodel.TUsers.ToList());
+            }
+
+        }
+
+        // GET: Tsignup/Details/5
+        public ActionResult Details(int id)
+        {
+            using (training_dbEntities dbModel = new training_dbEntities())
+            {
+                return View(dbModel.TUsers.Where(x => x.UserID == id).FirstOrDefault());
+            }
+
+        }
+
+        // GET: Tsignup/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tsignup/Create
+        [HttpPost]
+        public ActionResult Create(TUser customer)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+
+
+                using (training_dbEntities dbModel = new training_dbEntities())
+                {
+                    dbModel.TUsers.Add(customer);
+                    dbModel.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tsignup/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (training_dbEntities dbModel = new training_dbEntities())
+            {
+                return View(dbModel.TUsers.Where(x => x.UserID == id).FirstOrDefault());
+            }
+
+        }
+
+        // POST: Tsignup/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, TUser customer)
+        {
+            try
+            {
+                using (training_dbEntities dbModel = new training_dbEntities())
+                {
+                    dbModel.Entry(customer).State = EntityState.Modified;
+                    dbModel.SaveChanges();
+                }
+                // TODO: Add update logic here
+
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tsignup/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (training_dbEntities dbModel = new training_dbEntities())
+            {
+                return View(dbModel.TUsers.Where(x => x.UserID == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Tsignup/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (training_dbEntities dbModel = new training_dbEntities())
+                {
+                    TUser customer = dbModel.TUsers.Where(x => x.UserID == id).FirstOrDefault();
+                    dbModel.TUsers.Remove(customer);
+                    dbModel.SaveChanges();
+                }
+                // TODO: Add delete logic here
+
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
